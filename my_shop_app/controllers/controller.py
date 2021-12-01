452507494f403e86c00280258554f11c483de902
ccx1010sky <ccx1010@gmail.com>
2021-12-01 18:56:04 +0000

    deleted list2.html list3.html

--- my_shop_app/controllers/controller.py
+++ my_shop_app/controllers/controller.py
@@ -2,26 +2,26 @@ from app import app
 from flask import render_template
 from models.order_lsit import orders
 
+
+
+# Function name can be anything,
+#  assign "Food shop" to title variable in the index.html
+# assign orders object to orders variable in every html file.
 @app.route("/")
 
 def index():
-    return render_template("index.html",title = "Food shop")
+    return render_template("index.html",title = "Food shop1")
 
-# 
 @app.route("/orders")
 def order():
     return render_template("order.html",title = "orders",orders = orders)
 
-
-@app.route("/orders/list1")
-def list1():
-    return render_template("list1.html",title = "list1", orders = orders)
-@app.route("/orders/list2")
-def list2():
-    return render_template("list2.html",title = "list2", orders = orders)
-@app.route("/orders/list3")
-def list3():
-    return render_template("list3.html",title = "list3", orders = orders)
+# listindex type is string, listindex is parameter
+# assign orders[int(listindex)] to list_item variable,and pass to list_item argument.
+@app.route("/orders/<listindex>")
+def list1(listindex):
+    list_item = orders[int(listindex)]
+    return render_template("list1.html",title = "list1", list_item =list_item)
 
 
 
